7bbce21fc2f1958d55b4be622251a39e971aed5a
Scott VanFossen <scott.vanfossen@gmail.com>
2018-08-20 14:53:46 -0500

    scaffolding the express web app

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+// imports what app depends upon
+const express = require('express'); //express application defined
+const bodyParser = require('body-parser'); //middleware
+const cors = require('cors'); //middleware
+const helmet = require('helmet'); //middleware
+const morgan = require('morgan'); //middleware
+
+//define express app
+const app = express(); //application
+app.use(helmet()); //middleware
+app.use(bodyParser.json()); //middleware
+app.use(cors()); //middleware
+app.use(morgan('combined')); //middleware
+
+//listen for changes, live updating app
+app.listen(8081, () => console.log('listening on port 8081'));
